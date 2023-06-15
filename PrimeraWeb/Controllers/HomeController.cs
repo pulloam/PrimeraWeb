fccd643d9c1b091cb30a38b1bf6e685142a1fa3f
Pedro Ulloa <pulloam@hotmail.com>
2023-06-14 20:10:26 -0400

    Agregar archivos de proyecto.

---
+++ PrimeraWeb/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrimeraWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id, string nombre)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formulario) {
+            string nombre, apellido;
+
+            nombre = formulario["nombre"].ToString();
+            apellido = formulario["apellido"].ToString();
+
+            return Content("Llegaron los datos, nombre " + nombre + " apellido " 
+                + apellido);
+
+        }
+    }
+}
